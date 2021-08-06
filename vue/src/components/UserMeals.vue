d9db5e5f823eb9c1336985a29aa9e7c4a6b23e5e
Laura Julius <laurajulius@gmail.com>
2021-08-06 13:08:26 -0400

    added styling to user dashboard

--- vue/src/components/UserMeals.vue
+++ vue/src/components/UserMeals.vue
@@ -3,7 +3,7 @@
   <div class="glass">
       <p v-bind="getMyMeals()">meals</p>
       <p>{{meals}}</p>
-            <!-- <div class="dashboard">
+            <div class="dashboard">
                 <div class="user">
                     <h2>Your Meal Plans</h2>
                     <div class="links">
@@ -45,7 +45,7 @@
                         </div>
                     </div>
                 </div>
-            </div> -->
+            </div>
         </div>
 </template>
 
@@ -72,5 +72,100 @@ data() {
 
 </script>
 <style  scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;900&display=swap');
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins';
+    
+}
+
+main {
+    min-height: 100vh;
+    background: url('/assets/ella-olsson-sosOqjx31Go-unsplash.jpg');
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-size: contain;
+}
+
+.glass {
+    background: #fff;
+    min-height: 80vh;
+    width: 90%;
+    background: linear-gradient(to right bottom, rgba(255, 255, 255, 0.7), rgba(226, 226, 226, 0.3));
+    border-radius: 2rem;
+    backdrop-filter: blur(10rem);
+    display: flex;
+    border: 1px solid rgba(171, 161, 164, 1);
+}
+
+.dashboard {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    text-align: center;
+    background: linear-gradient(to right bottom, rgba(255, 255, 255, 0.7), rgba(226, 226, 226, 0.3));
+    border-radius: 2rem;  
+}
+
+.link {
+    display: flex;
+    margin: 2rem 0rem;
+    padding: 1rem 5rem;
+    align-items: center;
+    
+}
+
+.link a, h1, h2{
+    padding: 0rem 2rem;
+    text-decoration: none;
+    color: rgba(171, 161, 164, 1);
+    text-transform: uppercase;
+    font-weight: bold;
+}
+
+.link a:hover {
+    color: #000;
+    
+}
+
+.meal-plans {
+    flex: 2;
+}
+
+.search-btn {
+    color:#fff;
+    background: #ABA1A4;
+    text-decoration: none;
+    font-weight: 700;
+    letter-spacing: 2px;
+    border-radius: 5px;
+    padding: 10px 20px;
+    justify-content: center;
+    transition: 0.3s;
+    transition-property: background;
+}
+
+.search-btn:hover{
+    background-color: #000;
+}
+
+.meal-info a{
+    text-decoration: none;
+    color: #ABA1A4;
+}
+
+.meal-info a:hover {
+    color: #000;
+}
+
+.meal-info {
+    margin: 5rem;
+}
 
 </style>
