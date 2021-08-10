e338e7e41d605f130a10250ecda7c52f58f3cb1e
Laura Julius <laurajulius@gmail.com>
2021-08-10 17:51:10 -0400

    my recipe view

---
+++ vue/src/components/MyRecipesCards.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class = "my-recipe-card">
+        <div v-for="recipe in myRecipes" v-bind:key="recipe.id"></div>
+        <p>{{recipe.recipeName}}</p>
+    </div>
+</template>
+
+
+<script>
+
+
+
+export default {
+    name: "my-recipes-cards",
+    props: ["myRecipes"]
+    
+}
+</script>
+
+
+
+
+<style>
+
+.my-recipe-card {
+  margin-top: 300px;
+}
+
+</style>
